e2f6490f98bd3cd256522e566c54807015338812
ihksan.sukmawan <ihksan.sukmawan@mncgroup.com>
2023-05-06 11:37:16 +0700

    initial commit

---
+++ Star Wars Database/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Star Wars Database
+//
+//  Created by MCOMM00008 on 06/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
